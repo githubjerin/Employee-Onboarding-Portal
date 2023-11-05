6a4c3af4180dc919858f64e38e2a387134bf6e3d
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2023-11-05 09:30:51 +0530

    update navbar

--- client/src/pages/navBarComponent.jsx
+++ client/src/pages/navBarComponent.jsx
@@ -3,9 +3,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar'
 // import NavDropdown from 'react-bootstrap/NavDropdown';
-import {BiSolidUser,BiSolidUserPlus} from 'react-icons/bi';
+import {BiLogOut} from 'react-icons/bi';
 import {BsBriefcaseFill} from 'react-icons/bs';
 import {FaPeopleGroup} from 'react-icons/fa6';
+import {Landing} from './welcomeUser.page';
 
 const st={
   backgroundColor: "transparent" 
@@ -16,17 +17,16 @@ function ShowNavigationBar(){
         <Navbar.Brand href="#home" className = 'fst-italic'>Essential Weeds</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav fill variant="tabs" defaultActiveKey="/PageRender" className="me-auto">  
-            <Nav.Link href="/PageRender" className='fw-bold'>Dashboard</Nav.Link>
+          <Nav fill variant="tabs" defaultActiveKey=" " lassName="me-auto">  
+            <Nav.Link href="/WelcomePage" className='fw-bold'>Dashboard</Nav.Link>
             <Nav.Link href="/FormInput" className='fw-bold'>Onboarding<BsBriefcaseFill style = {{marginLeft : '5px'}}/></Nav.Link>
             <Nav.Link href="#link" className='fw-bold'>Members<FaPeopleGroup style = {{marginLeft : '5px'}}/></Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href = "/" className='fw-bold'>Login<BiSolidUser style = {{fontSize : '20px',marginLeft : '5px'}}/></Nav.Link>
-            <Nav.Link href = "/signup" className='fw-bold'>Signup<BiSolidUserPlus style = {{fontSize : '27px',marginLeft : '5px'}}/></Nav.Link>
+            <Nav.Link href = "/" className='fw-bold'>Logout<BiLogOut style = {{fontSize : '20px',marginLeft : '5px'}}/></Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        </Container>
+        </Container>    
     </Navbar>
 }
-export default ShowNavigationBar;
+export default ShowNavigationBar;   
